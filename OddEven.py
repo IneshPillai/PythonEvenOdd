891addab43d13abe019349fab20df00deffe8aa6
suraj <sg31710@gmail.com>
2016-10-08 11:16:00 +0530

    Odd Even Program

--- OddEven.py
+++ OddEven.py
@@ -1,6 +1,8 @@
-num=int(input("Enter a Number"))
-if num%2==0:
+def isEven(number):
+      if num%2==0:
            print("The Number is EVEN")
-else:
+      else:
            print("The Number is ODD")
-           
+
+num=int(input("Enter a number"))
+isEven(num)
